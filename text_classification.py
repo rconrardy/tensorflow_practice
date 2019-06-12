012422481984c12ba50f9e3c8aae70a84f4414bc
rconrardy <conrardy@umich.edu>
2019-06-12 10:27:59 -0400

    Finished text classification for imdb and started regression example

--- text_classification.py
+++ text_classification.py
@@ -22,14 +22,14 @@ reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
 def decode_review(text):
     return ' '.join([reverse_word_index.get(i, '?') for i in text])
 
-x_train = tf.keras.preprocessing.sequence.pad_sequence(
+x_train = tf.keras.preprocessing.sequence.pad_sequences(
     x_train,
     value=word_index["<PAD>"],
     padding='post',
     maxlen=256
 )
 
-x_test = tf.keras.preprocessing.sequence.pad_sequence(
+x_test = tf.keras.preprocessing.sequence.pad_sequences(
     x_test,
     value=word_index["<PAD>"],
     padding='post',
@@ -38,8 +38,69 @@ x_test = tf.keras.preprocessing.sequence.pad_sequence(
 
 vocab_size = 10000
 
-model = tf.keras.Sequential(
-    tf.keras.layers.Embedding(vocab_size, 16)
-    tf.keras.layers.GlobalAveragePooling1D()
-    
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Embedding(vocab_size, 16))
+model.add(tf.keras.layers.GlobalAveragePooling1D())
+model.add(tf.keras.layers.Dense(16, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(1, activation=tf.nn.sigmoid))
+
+print(model.summary())
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['acc']
 )
+
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+history = model.fit(
+    partial_x_train,
+    partial_y_train,
+    epochs=40,
+    batch_size=512,
+    validation_data=(x_val, y_val),
+    verbose=1
+)
+
+results = model.evaluate(x_test, y_test)
+
+print(results)
+
+history_dict = history.history
+print(history_dict.keys())
+
+import matplotlib.pyplot as plt
+
+acc = history_dict['acc']
+val_acc = history_dict['val_acc']
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+
+epochs = range(1, len(acc) + 1)
+
+# "bo" is for "blue dot"
+plt.plot(epochs, loss, 'bo', label='Training loss')
+# b is for "solid blue line"
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+plt.clf()   # clear figure
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.show()
