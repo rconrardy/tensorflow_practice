3ad106d9b28bf1951c1761e9dd74e059383392f8
rconrardy <conrardy@umich.edu>
2019-06-13 16:27:09 -0400

    added self check for trying to transfer to gpu tensorflow, also continued work on text generation

--- text_generation_rnn.py
+++ text_generation_rnn.py
@@ -11,3 +11,75 @@ text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
 print('Length of text: {} characters'.format(len(text)))
 
 print(text[:250])
+
+vocab = sorted(set(text))
+print ('{} unique characters'.format(len(vocab)))
+
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+
+text_as_int = np.array([char2idx[c] for c in text])
+
+print('{')
+for char,_ in zip(char2idx, range(20)):
+    print('  {:4s}: {:3d},'.format(repr(char), char2idx[char]))
+print('  ...\n}')
+
+print ('{} ---- characters mapped to int ---- > {}'.format(repr(text[:13]), text_as_int[:13]))
+
+seq_length = 100
+examples_per_epoch = len(text)
+
+print(examples_per_epoch)
+
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+for i in char_dataset.take(5):
+    print(idx2char[i.numpy()])
+
+sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+
+for item in sequences.take(5):
+    print(repr(''.join(idx2char[item.numpy()])))
+
+def split_input_target(chunk):
+    input_text = chunk[:-1]
+    target_text = chunk[1:]
+    return input_text, target_text
+
+dataset = sequences.map(split_input_target)
+
+for input_example, target_example in dataset.take(1):
+    print('Input data: ', repr(''.join(idx2char[input_example.numpy()])))
+    print('Target data: ', repr(''.join(idx2char[target_example.numpy()])))
+
+for i, (input_idx, target_idx) in enumerate(zip(input_example[:5], target_example[:5])):
+    print("Step {:4d}".format(i))
+    print("  input: {} ({:s})".format(input_idx, repr(idx2char[input_idx])))
+    print("  expected output: {} ({:s})".format(target_idx, repr(idx2char[target_idx])))
+
+BATCH_SIZE = 64
+steps_per_epoch = examples_per_epoch // BATCH_SIZE
+
+BUFFER_SIZE = 10000
+
+dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+
+print(dataset)
+
+vocab_size = len(vocab)
+
+embedding_dim = 256
+
+rnn_units = 1024
+
+from tensorflow.python.client import device_lib
+def get_available_devices():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos]
+print(get_available_devices()) 
+
+tf.test.gpu_device_name()
+
+if tf.test.is_gpu_available():
+    print("yep")
